3ca27010e5a7a2f19f72988dd8b2ae083e286b14
SSOL7 <soltani.shaho@gmail.com>
2023-01-31 10:58:00 +0200

    temperature converter

--- Desktop/BC/JS/Margit/Gas-calculator/index.js
+++ Desktop/BC/JS/Margit/Gas-calculator/index.js
@@ -1,19 +1,3 @@
-// function computeprice(){
-//   let price = document.getElementById('price').value;
-//   let fuel = document.getElementById('money').value;
-//   let costper = document.getElementById('costper').value;
-//   let total =  (fuel * (price)).toFixed(2);
-//   let costper100 = (total * costper).toFixed(2);
-
-//   if(total === 20){
-//     document.getElementById('message').textContent = "You are paying too much for fuel";
-//   }
-
-//   document.getElementById('consumption').textContent = "Total Fuel cost = $ " + total;
-//   // calculate the total fuel cost
-//     // document.getElementById('consumption').textContent = ""+ costper100 + "Total Fuel cost = $ " + (fuel*costper).toFixed(2);
-//   }
-
 const calcGasoline = () => {
   const price = Number(document.getElementById('price').value);
   const money = +document.querySelector('#money').value;
@@ -28,10 +12,12 @@ const calcGasoline = () => {
   } else {
     text = `you need to buy more fuel, at least ${210 - amount} more`;
   }
-
   answer.textContent = text;
+};
 
-
-  console.log(price, money);
+const function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputFahrenheit").innerHTML=(valNum*1.8)+32;
 }
+
 
